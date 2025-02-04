91c6d4c292356d48f693c35fbd684ff9358fd49c
Prem-Ray <premanshuray981@gmail.com>
2025-02-04 21:45:00 +0530

    first commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
+function formSub(e){
+  e.preventDefault();
+}
+
 function Contact() {
 
-  function formSub(e){
-    e.preventDefault();
-  }
   return (
     <section
       id="contact"
@@ -18,7 +19,7 @@ function Contact() {
         </h2>
         <div className="flex flex-col justify-center items-center">
           <div className="w-full md:w-2/3 lg:w-1/2 xl:w-1/3 px-4 mb-8">
-            <form onSubmit={formSub} action="#">
+            <form onSubmit={formSub}>
               <div className="flex flex-col sm:flex-row sm:space-x-4 mb-4">
                 <input
                   type="text"
